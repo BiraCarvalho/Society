fb7085c0893ec3514bbfbbdd71090d1b40729e0f
Ubiratã Carvalho <birah.carvalho@gmail.com>
2024-09-29 21:41:52 -0300

    Adicionei o controller de socios com listagem e criação

--- app/src/Controller/Api/SocioController.php
+++ app/src/Controller/Api/SocioController.php
@@ -2,18 +2,38 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Socio;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SocioController extends AbstractController
 {
-    #[Route('/api/socio', name: 'app_api_socio')]
-    public function index(): JsonResponse
+    #[Route('/api/socios', methods:['GET'])]
+    public function index(EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Api/SocioController.php',
-        ]);
+        return $this->json(
+            $entityManager->getRepository(Socio::class)->findAll()
+        );
+    }
+
+    #[Route('/api/socio', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $socio = new Socio();
+        $socio->setNome($data['nome']);
+        $socio->setCpf($data['cpf']);
+
+        $entityManager->persist($socio);
+        $entityManager->flush();
+
+        return new JsonResponse(
+            ['status' => 'Sócio criado'],
+            JsonResponse::HTTP_CREATED
+        );
     }
 }
